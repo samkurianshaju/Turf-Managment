35edc66c860e96fe0fc6591f3df8e8f8077a6f5e
samkurianshaju <68271097+samkurianshaju@users.noreply.github.com>
2023-02-01 20:44:41 +0530

    backend completed

--- controllers/booking-controllers.js
+++ controllers/booking-controllers.js
@@ -86,12 +86,13 @@ const getById = async(req,res,next) =>{
    return res.status(200)
    .json({booking});
 };
-
  const deleteBooking = async(req,res,next) => {
    const id=req.params.id;
    let booking;
    try{
-      booking=await Booking.findByIdAndRemove(id)
+      booking=await Booking.findByIdAndRemove(id).populate('user');
+      await booking.user.bookings.pull(booking);
+      await booking.user.save();
    }catch(err){
       return console.log(err);
    }
@@ -102,8 +103,23 @@ const getById = async(req,res,next) =>{
    return res.status(200)
    .json({message:"Booking deleted successfully"});
  };
+
+ const getByUserId = async(req,res,next) =>{
+   const userId=req.params.id;
+   let userBookings;
+   try{
+      userBookings=await User.findById(userId).populate("bookings");
+   }catch(err){
+      return console.log(err);
+   }
+   if(!userBookings){
+      return res.status(404).json({message:"No bookings found"})
+   }
+   return res.status(200).json({bookings:userBookings});
+ };
 exports.addBooking=addBooking;
 exports.getAllBookings=getAllBookings;
 exports.updateBooking=updateBooking;
 exports.getById=getById;
 exports.deleteBooking=deleteBooking;
+exports.getByUserId=getByUserId;
--- routes/booking-routes.js
+++ routes/booking-routes.js
@@ -1,5 +1,5 @@
 const express= require("express");
-const { getAllBookings, addBooking, updateBooking, getById, deleteBooking } = require("../controllers/booking-controllers");
+const { getAllBookings, addBooking, updateBooking, getById, deleteBooking, getByUserId } = require("../controllers/booking-controllers");
 const Booking = require("../model/Booking");
 const bookrouter = express.Router();
 //const bookingController = require("../controllers/booking-controllers");
@@ -9,6 +9,7 @@ bookrouter.post("/add",addBooking);
 bookrouter.put("/update/:id",updateBooking);
 bookrouter.get("/:id",getById);
 bookrouter.delete("/:id",deleteBooking);
+bookrouter.get("/user/:id",getByUserId);
 //router.post("/",addBooking);
 
 module.exports = bookrouter;
